0865a70cfac226584ad6670706682da8f94cae42
Sebastian Berning <hunter.Vasitas@googlemail.com>
2020-05-02 13:24:22 +0200

    Added message interface

---
+++ ValkyrEngine.MessageQueue/IMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace ValkyrEngine.MessageQueue
+{
+  /// <summary>
+  /// Represents an object, which can be handled in the message system and can be used to transfer information.
+  /// </summary>
+  public interface IMessage
+  {
+  }
+}
