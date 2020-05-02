d7b44675fcb421f59f82001e4e8c22b61ae769c3
Sebastian Berning <hunter.Vasitas@googlemail.com>
2020-05-03 01:50:23 +0200

    adding autofac module

---
+++ ValkyrEngine.MessageSystem/MessageSystemModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+
+namespace ValkyrEngine.MessageSystem
+{
+  /// <summary>
+  /// A class that is used to setup autofac.
+  /// </summary>
+  public class MessageSystemModule : Module
+  {
+    /// <inheritdoc/>
+    protected override void Load(ContainerBuilder builder)
+    {
+      builder.RegisterType<MessageSystem>()
+             .As<IMessageSystem>()
+             .SingleInstance();
+    }
+  }
+}
