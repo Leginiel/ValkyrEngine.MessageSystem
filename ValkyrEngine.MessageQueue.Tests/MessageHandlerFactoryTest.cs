0c85495ae4abf88de4333c2a392cbc3f8562336f
Sebastian Berning <hunter.Vasitas@googlemail.com>
2020-05-02 13:24:36 +0200

    added message handler factory

---
+++ ValkyrEngine.MessageQueue.Tests/MessageHandlerFactoryTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ValkyrEngine.MessageQueue.Tests
+{
+  public class MessageHandlerFactoryTest
+  {
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void CreateMessageHandler_ValidCallback_IMessageHandler()
+    {
+      // Arrange
+      MessageHandlerFactory factory = new MessageHandlerFactory();
+      IMessageHandler messageHandler;
+      // Act
+      messageHandler = factory.CreateMessageHandler<IMessage>(HandleMessage);
+
+      // Assert
+      Assert.NotNull(messageHandler);
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void CreateMessageHandler_Null_ArgumentNullException()
+    {
+      // Arrange
+      MessageHandlerFactory factory = new MessageHandlerFactory();
+
+      // Act / Assert
+
+      Assert.Throws<ArgumentNullException>(() => factory.CreateMessageHandler<IMessage>(null));
+    }
+
+    private Task HandleMessage(IMessage obj)
+    {
+      return new Task(() => { });
+    }
+  }
+}
---
+++ ValkyrEngine.MessageQueue/MessageHandlerFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ValkyrEngine.MessageQueue
+{
+  internal sealed class MessageHandlerFactory
+  {
+    public IMessageHandler CreateMessageHandler<T>(Func<T, Task> callback)
+      where T : IMessage
+    {
+      if (callback == null)
+        throw new ArgumentNullException(nameof(callback));
+
+      return new MessageHandler<T>(callback);
+    }
+  }
+}
